c9dc6700806a43f4518e80443866af763b3ecae5
Theodor Stana <stana@kth.se>
2019-02-14 10:24:04 +0100

    Adding cubes_timekeeping drivers
    Along with some quick testing in main.c

--- firmware/cubes_hw_platform.h
+++ firmware/cubes_hw_platform.h
@@ -2,7 +2,7 @@
 #define cubes_HW_PLATFORM_H_
 /*****************************************************************************
 *
-*Created by Microsemi SmartDesign  Mon Jan 07 15:59:03 2019
+*Created by Microsemi SmartDesign  Wed Feb 13 10:06:30 2019
 *
 *Memory map specification for peripherals in cubes
 */
@@ -11,6 +11,8 @@
 * CM3 subsystem memory map
 * Master(s) for this subsystem: CM3 
 *---------------------------------------------------------------------------*/
+#define CUBES_TIMEKEEPING               0x50000000U
+#define CITIROC_INTF                    0x50001000U
 
 
 #endif /* cubes_HW_PLATFORM_H_*/
--- main.c
+++ main.c
@@ -9,14 +9,31 @@
 
 #include "msp/msp_i2c.h"
 #include <stdint.h>
+
+#include "firmware/drivers/cubes_timekeeping/cubes_timekeeping.h"
+
 #include "hvps/hvps_c11204-02.h"
 #include "msp/msp_exp.h"
-
 #include "mem_mgmt/mem_mgmt.h"
+
 #define SLAVE_ADDR 0x35
 #define nvm_mem_addr 0x60000000
 
 int main(void){
+	uint32_t current_time, i;
+
+	for (i = 0; i < 1000000; ++i)
+		;
+	current_time = cubes_get_time();
+
+	for (i = 0; i < 1000000; ++i)
+		;
+	current_time = cubes_get_time();
+
+	/* Test code, skip the rest! */
+	while (1)
+		;
+
 	mem_ram_write(RAM_HVPS, "0000000000000000746900C8"); /* Writing standard HVPS value to ram for testing */
 	msp_read_seqflags();
 	init_i2c(SLAVE_ADDR);

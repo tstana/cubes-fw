5cbbf686da738f537086ea6b16fc222e23a3c58a
Theodor Stana <stana@kth.se>
2019-02-14 12:19:05 +0100

    Small changes to cubes_timekeeping test code
    The test code (in main.c) for the cubes_timekeeping module now sets various
    time values and reads them later for checking. The checking process is very
    manual, see the comments at the top of the while(1) statement in main.c
    for details.

--- main.c
+++ main.c
@@ -20,19 +20,27 @@
 #define nvm_mem_addr 0x60000000
 
 int main(void){
-	uint32_t current_time, i;
+	uint32_t current_time;
 
-	for (i = 0; i < 1000000; ++i)
-		;
-	current_time = cubes_get_time();
-
-	for (i = 0; i < 1000000; ++i)
-		;
-	current_time = cubes_get_time();
-
-	/* Test code, skip the rest! */
-	while (1)
-		;
+	/*
+	 * Test code: set various values of time and check that they are set.
+	 *
+	 * At each breakpoint, _mentally_ count the number of seconds passed between
+	 * the time when the breakpoint hits and when you click "Run" in the Debug
+	 * window. When the next breakpoint hits, check that the current_time value
+	 * is according to the mental count. Then repeat.
+	 *
+	 * The rest of the function is virtually skipped for now.
+	 */
+	while (1) {
+		current_time = cubes_get_time();
+		cubes_set_time(0);
+		current_time = cubes_get_time();
+		cubes_set_time(60);
+		current_time = cubes_get_time();
+		cubes_set_time((uint32_t)0x0fffffff);
+		current_time = cubes_get_time();
+	}
 
 	mem_ram_write(RAM_HVPS, "0000000000000000746900C8"); /* Writing standard HVPS value to ram for testing */
 	msp_read_seqflags();

8840dd3a4a1b8ab36bf2068c57b8724b11a6e6b1
Theodor Stana <stana@kth.se>
2020-10-31 16:07:12 +0100

    Change I2C RX and TX buffer sizes in msp_i2c.c

--- msp/msp_i2c.c
+++ msp/msp_i2c.c
@@ -17,21 +17,48 @@
 #include "msp_i2c.h"
 
 
-uint8_t i2c_tx_buffer[550] = "";
-uint8_t i2c_rx_buffer[550] = "";
-uint32_t slave_buffer_size = 550;
+/*
+ * Define RX and TX buffers for I2C; their sizes are the max. data that is to
+ * be sent via MSP. For the RX buffer, this is perhaps strictly not needed, but
+ * better to avoid a HardFault in case the OBC tries to send CUBES more data
+ * than it expects.
+ *
+ * Note: If the OBC tries to send it more than the MTU is another matter!
+ */
+static uint8_t i2c_tx_buffer[MSP_EXP_MAX_FRAME_SIZE];
+static uint8_t i2c_rx_buffer[MSP_EXP_MAX_FRAME_SIZE];
 
-static mss_i2c_slave_handler_ret_t slave_write_handler(mss_i2c_instance_t * this_i2c,uint8_t * p_rx_data,uint16_t rx_size){
+static uint32_t slave_buffer_size = 0;
+
+/*
+ *
+ */
+static
+mss_i2c_slave_handler_ret_t slave_write_handler(mss_i2c_instance_t * this_i2c,
+                                                uint8_t * p_rx_data,
+                                                uint16_t rx_size)
+{
 	msp_recv_callback(p_rx_data, rx_size);
 	msp_send_callback((unsigned char *)i2c_tx_buffer, (unsigned long *)&slave_buffer_size);
 	return MSS_I2C_REENABLE_SLAVE_RX;
 }
 
-void msp_init_i2c(int slave_ser_addr){
-	MSS_I2C_init(&g_mss_i2c1, slave_ser_addr, MSS_I2C_PCLK_DIV_60); /* Clock can be set to any value as slave, not used */
-	MSS_I2C_set_slave_tx_buffer(&g_mss_i2c1, i2c_tx_buffer, sizeof(i2c_tx_buffer)); /* Set buffers for RX and TX I2C */
+/**
+ * @brief Initialize MSS_I2C1 for use with MSP
+ *
+ * @param slave_ser_addr   I2C slave address of CUBES, set it to `MSP_EXP_ADDR`
+ */
+void msp_init_i2c(int slave_ser_addr)
+{
+	/* `ser_clock_speed` param not important, CUBES is an I2C slave... */
+	MSS_I2C_init(&g_mss_i2c1, slave_ser_addr, MSS_I2C_PCLK_DIV_60);
+	MSS_I2C_set_slave_tx_buffer(&g_mss_i2c1, i2c_tx_buffer, sizeof(i2c_tx_buffer));
 	MSS_I2C_set_slave_rx_buffer(&g_mss_i2c1, i2c_rx_buffer, sizeof(i2c_rx_buffer));
-	MSS_I2C_clear_gca(&g_mss_i2c1); /* Can not be adressed by a general call adress */
+
+	/* CUBES can not be adressed by a general call adress */
+	MSS_I2C_clear_gca(&g_mss_i2c1);
+
+	/* Register local write handler function and enable slave */
 	MSS_I2C_register_write_handler(&g_mss_i2c1, slave_write_handler);
 	MSS_I2C_enable_slave(&g_mss_i2c1);
 }

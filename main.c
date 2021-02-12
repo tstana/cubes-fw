86c6d196ddeed9af11e9273ec48c57f1fd7eb9b2
sonals <sonals@kth.se>
2021-02-12 12:00:36 +0100

    renamed and de-parameterized led_blink() in the main(), replaced it with led_blink_reset()

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ int main(void)
 //	nvm_restore_msp_seqflags();
 
 	/* Let GPIO0 LED flash to indicate power-on status */
-	led_blink(LED_RESET);
+	led_blink_reset();
 
 	msp_i2c_init(MSP_EXP_ADDR);
 
@@ -60,6 +60,9 @@ int main(void)
 	MSS_TIM1_load_immediate(0xffffffff);
 	MSS_TIM1_enable_irq();
 	NVIC_SetPriority(Timer1_IRQn, 1);
+
+	/*For testing*/
+//	led_custom_blink(LED_OTHER_ERROR);
 
 	/* Infinite loop */
 	while(1) {

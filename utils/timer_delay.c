f14ff05b67ee707cd8d796f987f7a72d2fe50ada
sonals <sonals@kth.se>
2021-02-26 18:30:47 +0100

    rename file and fns names and code cleanup

--- utils/timer_delay.c
+++ utils/timer_delay.c
@@ -1,5 +1,5 @@
 /*
- * tim_delay.c
+ * timer_delay.c
  *
  *  Created on: 19-Feb-2021
  *      Author: Sonal Shrivastava
@@ -9,57 +9,43 @@
 
 #include "drivers/mss_timer/mss_timer.h"
 #include "CMSIS/system_m2sxxx.h"
-#include "tim_delay.h"
+#include "timer_delay.h"
 #include "led.h"
 
 
-// Delay counter
-//static volatile uint32_t DelayCounter;
-volatile uint32_t DelayCounter;
+volatile uint32_t delay_counter;
 
 
 void SysTick_Handler(void)
 {
-    DelayCounter++;
+    delay_counter++;
 }
 
 
-void tim_init(void)
+void timer_delay_init(void)
 {
-    // Set reload register to generate an interrupt every millisecond.
+    // Set reload register to generate an interrupt every millisecond
     SysTick->LOAD = (uint32_t)((SystemCoreClock / 1000) - 1);
 
-    // Set priority for SysTick IRQ
-    //NVIC_SetPriority(SysTick_IRQn,(1 << __NVIC_PRIO_BITS) - 1);
-
-    // Reset the SysTick counter value.
+    // Reset the SysTick counter value
     SysTick->VAL = 0UL;
 
-    // Set SysTick source and IRQ.
+    // Set SysTick source and IRQ
     SysTick->CTRL = (SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk);
 }
 
 
 
 
-void delay(uint32_t ms)
+void timer_delay(uint32_t ms)
 {
     // Enable the SysTick timer
     SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
 
     // Wait for a specified number of milliseconds
-    DelayCounter = 0;
-    while (DelayCounter < ms);
+    delay_counter = 0;
+    while (delay_counter < ms);
 
     // Disable the SysTick timer
     SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
-}
-
-
-void delay_test(void)
-{
-    tim_init();
-    led_turn_off();
-    delay(500);
-    led_turn_on();
 }
---
+++ utils/timer_delay.h
@@ -0,0 +1,15 @@
+/*
+ * timer_delay.h
+ *
+ *  Created on: 19-Feb-2021
+ *      Author: Sonal Shrivastava
+ */
+
+#ifndef UTILS_TIMER_DELAY_H_
+#define UTILS_TIMER_DELAY_H_
+
+void timer_delay_init(void);
+void timer_delay(uint32_t ms);
+
+
+#endif /* UTILS_TIMER_DELAY_H_ */

127151bf4957c3b5be96d7ab98bb832a099f280a
Tzu-Chiao Lin <74283099+tclin0122@users.noreply.github.com>
2023-05-17 16:55:55 +0200

    Add files via upload
    MSP update

--- msp/msp_opcodes.h
+++ msp/msp_opcodes.h
@@ -1,12 +1,12 @@
-/*
- * msp_opcodes.h
- * Authors: John Wikman
- *          Marcus Persson
- *          Theodor-Adrian Stana
- *          Ibrahim Hamed
- *          Patrik Johansson
+/**
+ * @file      msp_opcodes.h
+ * @author    John Wikman
+ * @copyright MIT License
+ * @brief     Defines all the standard opcodes in MSP.
  *
- * Defines all the CUBES MSP opcodes.
+ * @details
+ * Defines all the standard opcodes in MSP as well as macros for categorizing
+ * opcodes.
  */
 
 #ifndef MSP_OPCODES_H
@@ -58,8 +58,20 @@
 #define MSP_OP_TYPE_REQ  0x20
 #define MSP_OP_TYPE_SEND 0x30
 
+/**
+ * @brief Determines the opcode type.
+ * @param opcode The opcode value.
+ * @return The type of the opcode. It will return either MSP_OP_TYPE_CTRL,
+ *         MSP_OP_TYPE_SYS, MSP_OP_TYPE_REQ, or MSP_OP_TYPE_SEND.
+ */
 #define MSP_OP_TYPE(opcode) ((opcode) & 0x30)
 
+/**
+ * @brief Determines whether the opcode is custom or not.
+ * @param opcode The opcode value.
+ * @return A non-zero value if the value represents a custom opcode. Otherwise
+ *         0 is returned.
+ */
 #define MSP_OP_IS_CUSTOM(opcode) (((opcode) & 0x70) >= 0x50)
 
-#endif
+#endif /* MSP_OPCODES_H */
